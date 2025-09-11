01dd8502d118fb9725935cdd2c488209342af102
AVBlana <vataselu.alex@yahoo.com>
2025-09-11 21:03:32 +0300

    working on spotify login

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -28,7 +28,6 @@ export default function SignInPage() {
   if (isAuthenticated) {
     return null;
   }
-  alert(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
